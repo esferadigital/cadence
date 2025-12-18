81fb6aa0fdc0bf374ded8f2385f3d1931ba26aaf
esferadigital <esferadigital@proton.me>
2025-12-18 12:32:21 -0500

    add notifier, listen to events sent by timer through channels

--- internal/timer/timer.go
+++ internal/timer/timer.go
@@ -5,6 +5,8 @@ import (
 	"time"
 )
 
+// ---- status ----
+
 type TimerStatus int
 
 const (
@@ -14,12 +16,48 @@ const (
 	StatusFinished
 )
 
-type PhaseType int
+// ---- phases ----
+
+type PhaseKind int
 
 const (
-	PhaseWork PhaseType = iota
+	PhaseWork PhaseKind = iota
 	PhaseBreak
 )
+
+type PhaseDetail struct {
+	Kind     PhaseKind
+	Duration time.Duration
+}
+
+// ---- events ----
+
+type EventKind int
+
+const (
+	Tick EventKind = iota
+	PhaseFinished
+	TimerFinished
+)
+
+// Data sent with event
+type EventData struct {
+	// Sent for `Tick` and `PhaseFinished`
+	PhaseIdx int
+
+	// Sent for `Tick`
+	PhaseRemaining time.Duration
+}
+
+type TimerEvent struct {
+	// Always sent
+	Kind EventKind
+
+	// Sent for `Tick` and `PhaseFinished`
+	Data EventData
+}
+
+// ---- timer ----
 
 type Timer struct {
 	workDur       time.Duration
@@ -29,7 +67,7 @@ type Timer struct {
 	phaseElapsed  time.Duration
 	phaseLastTick time.Time
 	status        TimerStatus
-	// sender
+	events        chan TimerEvent
 }
 
 func New(workDur time.Duration, breakDur time.Duration, workPhases int) Timer {
@@ -39,7 +77,12 @@ func New(workDur time.Duration, breakDur time.Duration, workPhases int) Timer {
 		phaseCnt: (workPhases * 2) - 1,
 		phaseIdx: 0,
 		status:   StatusInit,
+		events:   make(chan TimerEvent),
 	}
+}
+
+func (t *Timer) Events() <-chan TimerEvent {
+	return t.events
 }
 
 func (t *Timer) Start() {
@@ -54,57 +97,83 @@ func (t *Timer) Start() {
 // - Handle case where the timer is not actually running
 // - Handle case where the timer's last tick is at it's default value
 func (t *Timer) Tick() {
+	log.Println("tick, status:", t.status)
 	delta := time.Since(t.phaseLastTick)
 	for delta > 0 {
-		var phaseType PhaseType
-		var phaseDur time.Duration
-		if t.phaseIdx%2 == 0 {
-			phaseType = PhaseWork
-			phaseDur = t.workDur
-		} else {
-			phaseType = PhaseBreak
-			phaseDur = t.breakDur
-		}
+		phase := t.phaseDetail()
 
 		// Handle ticks that occur while we are still in the current phase duration
-		if delta+t.phaseElapsed < phaseDur {
+		if delta+t.phaseElapsed < phase.Duration {
 			t.phaseElapsed += delta
 			break
 		}
 
 		// Handle ticks that occur after (i.e. phase is finished)
-		// Subtract remaining phase duration from delta
-		delta -= phaseDur - t.phaseElapsed
-
-		finishedPhase := t.phaseIdx/2 + 1
-		if phaseType == PhaseWork {
-			// Send event that work is finished
-			log.Printf("Finished work phase %d", finishedPhase)
-		} else {
-			// Send event that break is finished
-			log.Printf("Finished break phase %d", finishedPhase)
-		}
-
-		// Advance to the next phase
-		t.phaseIdx += 1
-		t.phaseElapsed = time.Duration(0)
-
-		// Check if new phase is actually beyond the expected phases
-		if t.phaseIdx >= t.phaseCnt {
+		// Check if all the phases are complete
+		next := t.phaseIdx + 1
+		if next >= t.phaseCnt {
 			t.status = StatusFinished
-
-			// Send event that the timer is finished
-			log.Println("Time is finished; nice job")
-
+			event := TimerEvent{
+				Kind: TimerFinished,
+			}
+			t.events <- event
+			close(t.events)
 			return
 		}
+
+		// Send an event saying the phase has finished
+		data := EventData{
+			PhaseIdx: t.phaseIdx,
+		}
+		event := TimerEvent{
+			Kind: PhaseFinished,
+			Data: data,
+		}
+		t.events <- event
+
+		// Subtract remaining phase duration from delta
+		remaining := t.phaseRemaining()
+		delta -= remaining
+
+		// Advance to the next phase
+		t.phaseIdx = next
+		t.phaseElapsed = time.Duration(0)
 	}
 
+	data := EventData{
+		PhaseIdx:       t.phaseIdx,
+		PhaseRemaining: t.phaseRemaining(),
+	}
+	event := TimerEvent{
+		Kind: Tick,
+		Data: data,
+	}
+	t.events <- event
+
 	t.phaseLastTick = time.Now()
-	// TODO: Send timer tick event
-	log.Printf("Tick! %d", t.phaseElapsed)
 }
 
 func (t *Timer) IsFinished() bool {
 	return t.status == StatusFinished
 }
+
+func (t *Timer) phaseRemaining() time.Duration {
+	phase := t.phaseDetail()
+	return phase.Duration - t.phaseElapsed
+}
+
+func (t *Timer) phaseDetail() PhaseDetail {
+	detail := PhaseDetail{}
+	if t.phaseIdx%2 == 0 {
+		detail.Kind = PhaseWork
+		detail.Duration = t.workDur
+	} else {
+		detail.Kind = PhaseBreak
+		detail.Duration = t.breakDur
+	}
+	return detail
+}
+
+// func humanIdx(phase int) int {
+// 	return phase/2 + 1
+// }
